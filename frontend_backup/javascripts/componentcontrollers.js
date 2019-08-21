dbabab61806cc6d0ac37ce0cad2aadb757e39928
Mits <websitedemopage@gmail.com>
2019-08-21 15:22:32 +0530

    Test

---
+++ frontend_backup/javascripts/componentcontrollers.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        },
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    })
+});
